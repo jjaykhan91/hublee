84897ab36aeadb3990da7dc9bbe89cf282f3a7b7
jjaykhan91 <jjaykhan91@gmail.com>
2025-08-12 17:08:14 -0400

    saving for emulator test

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+Docs: token endpoint + headers + base URLs + scope come from Quran Foundation quick start; you must send x-auth-token and x-client-id with each call.
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hublee/main.dart';
+import 'package:hublee/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HubleeApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
